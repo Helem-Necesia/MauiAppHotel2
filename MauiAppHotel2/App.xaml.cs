05fe9dd05fb0b6c72e4e8ff40f22a0e76b3fd896
Helem Necesia <hellems2necesia_@hotmail.com>
2024-11-15 14:41:15 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppHotel2/App.xaml.cs
@@ -0,0 +1,23 @@
+﻿namespace MauiAppHotel2
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+
+            var widow = base.CreateWindow(activationState);
+            widow.Width = 400;
+            widow.Height = 600;
+            
+            return widow;
+        }
+
+    }
+}
